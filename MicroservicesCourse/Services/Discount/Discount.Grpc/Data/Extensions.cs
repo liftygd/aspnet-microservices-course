09f3a87b36708888eb1b14ce3da2025a9972021d
liftygd <karagodin.artem27@gmail.com>
2025-09-26 15:32:32 +0300

    Created Discount service with gRPC, started development of the DiscountService

---
+++ MicroservicesCourse/Services/Discount/Discount.Grpc/Data/Extensions.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Discount.Grpc.Data
+{
+    public static class Extensions
+    {
+        public static void EnsureMigrationOf<T>(this IApplicationBuilder app) where T : DbContext
+        {
+            using var scope = app.ApplicationServices.CreateScope();
+            using var context = scope.ServiceProvider.GetRequiredService<T>();
+
+            if (context == null)
+                throw new NotImplementedException($"Context of type '{typeof(T).Name}' was not added to application.");
+
+            context.Database.MigrateAsync();
+        }
+    }
+}
